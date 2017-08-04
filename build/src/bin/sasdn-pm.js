b47e5c6bab0f59dd01097e357a814df52c418f6b
冯洁 <fengjie@shinezone.com>
2017-08-04 10:12:47 +0800

    update

---
+++ build/src/bin/sasdn-pm.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const program = require("commander");
+const pkg = require('../../package.json');
+program.version(pkg.version)
+    .command('install [options]', 'install proto from spm server')
+    .command('list [options]', 'show all installed proto')
+    .command('publish [options]', 'publish proto dir to spm server')
+    .command('search [options]', 'search proto from spm server')
+    .command('secret [options]', 'set secret key in spm commander')
+    .command('uninstall [options]', 'install proto from spm server')
+    .parse(process.argv);
+//# sourceMappingURL=sasdn-pm.js.map
